4378c12ec3b4a596585fc5b3bec33a3236150536
badya367 <badikov_dima@mail.ru>
2023-12-09 19:43:25 +0300

    В пакете model создан класс Task, который представляет модель данных для задач. Определены поля, созданы конструкторы, реализованы гетеры и сетеры для полей, реализованы методы для отметки выполненой задачи и для проверки просроченной задачи

--- src/main/java/com/mytodolist/Application.java
+++ src/main/java/com/mytodolist/Application.java
@@ -1,8 +1,16 @@
 package com.mytodolist;
 
+import com.mytodolist.model.Task;
+
+import java.util.Date;
+
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("В этом проекте будет реализован мой список дел");
+
+        Task testTask = new Task("Задача 1", "Тестовая задача", "10.12.2023. 11:00");
+        testTask.setDueDate("13.12.2023. 11:00");
+        System.out.println(testTask.getDueDate());
     }
 }
--- src/main/java/com/mytodolist/model/Task.java
+++ src/main/java/com/mytodolist/model/Task.java
@@ -1,4 +1,93 @@
+/**
+ * Класс Task представляет собой модель данных для задач в приложении To-Do List.
+ */
+
 package com.mytodolist.model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Task {
+    private long id; //Уникальный идентификатор задачи
+    private String title; //Заголовок задачи
+    private String description; //Описание задачи
+    private Date dueDate; //Дата, к которой должна быть выполнена задача
+    private boolean completed; //Флаг, указывающий выполнена ли задача
+
+    public Task() {} //Конструктор по умолчанию
+
+    public Task(String title, String description, String dueDateString) { //Конструктор создаёт объект Task с заданными атрибутами
+
+        this.title = title;
+        this.description = description;
+        //Преобразование строки в объект Date
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy. HH:mm");
+        try {
+            this.dueDate = dateFormat.parse(dueDateString);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+        }
+        this.completed = false;
+
+    }
+
+    public void markAsCompleted() { //метод для отметки задания как выполненого
+        completed = true;
+    }
+
+    public boolean isOverdue() { //метод проверки просроченного задания
+        return dueDate != null && dueDate.before(new Date());
+    }
+
+    /*
+    --------------------
+    Гетеры и сетеры для полей класса Task
+    --------------------
+     */
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDateString) {
+        //Преобразование строки в объект Date
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy. HH:mm");
+        try {
+            this.dueDate = dateFormat.parse(dueDateString);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
