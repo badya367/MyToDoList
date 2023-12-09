3baeb385c43d9791b67c2b08d6e3ff58fab93895
badya367 <badikov_dima@mail.ru>
2023-12-09 23:05:16 +0300

    Подготовка интерфейса TaskRepository, класса TaskRepositoryImpl имплементирующего интерфейм, а также pom.xml файла для подключения и настройки БД MySQL

--- src/main/java/com/mytodolist/repository/TaskRepository.java
+++ src/main/java/com/mytodolist/repository/TaskRepository.java
@@ -1,4 +1,14 @@
 package com.mytodolist.repository;
 
-public class TaskRepository {
+import com.mytodolist.model.Task;
+
+import java.util.List;
+
+public interface TaskRepository {
+    Task save (Task task);
+    Task findById (long id);
+    List<Task> findAll();
+    List<Task> findByCompleted (boolean completed);
+    void update(Task task);
+    void delete(long id);
 }
---
+++ src/main/java/com/mytodolist/repository/TaskRepositoryImpl.java
@@ -0,0 +1,37 @@
+package com.mytodolist.repository;
+
+import com.mytodolist.model.Task;
+
+import java.util.List;
+
+public class TaskRepositoryImpl implements TaskRepository{
+    @Override
+    public Task save(Task task) {
+        return null;
+    }
+
+    @Override
+    public Task findById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Task> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Task> findByCompleted(boolean completed) {
+        return null;
+    }
+
+    @Override
+    public void update(Task task) {
+
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
+}
